68fb080ca9c7e54b21ecc0f9569f2df21f6c6afa
ScarMeireles <114191275+ScarMeireles@users.noreply.github.com>
2022-10-23 20:35:16 -0300

    Add files via upload

---
+++ Aula/src/exercicios/Spam.java
@@ -0,0 +1,9 @@
+package exercicios;
+
+public class Spam {
+        public static void main(String[] args) {
+            int i;
+            for (i=0;;i++)
+                System.out.println(i + "\n");
+        }
+}
